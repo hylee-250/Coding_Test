79511cafa264e16097e7891a064ccfeeb573e0ae
hylee <hylee5642@naver.com>
2021-08-13 20:20:11 +0900

    dfs problem 2

--- BOJ/DFS_BFS/virus.py
+++ BOJ/DFS_BFS/virus.py
@@ -1,1 +1,20 @@
-print("hello")
+def DFS(graph,root,visited=[]):
+    visited.append(root)
+    for node in graph[root]:
+        if node not in visited:
+            DFS(graph,node,visited)
+    return visited
+
+N = int(input())
+V = int(input())
+
+graph = [[] for x in range(N+1)]
+
+while V > 0:
+    start, end = map(int,input().split())
+    graph[start].append(end)
+    graph[end].append(start)
+    V -= 1
+
+result = DFS(graph,1)
+print(len(result)-1)
